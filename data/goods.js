17721d33d12aeb3f557903048003dfe66b4781df
Brittany Nicole <bnsquires1@gmail.com>
2024-05-27 13:40:20 -0400

    data

---
+++ data/goods.js
@@ -0,0 +1,76 @@
+// Goods/Services provided by each business.
+
+const goods = [
+    {
+      id: 1,
+      businessId: 1,
+      product: 'recipe guide',
+      description:
+        'similique esse doloribus nihil accusamus\nomnis dolorem fuga consequuntur reprehenderit fugit recusandae temporibus\nperspiciatis cum ut laudantium\nomnis aut molestiae vel vero',
+    },
+    {
+      id: 2,
+      userId: 1,
+      title: 'at home mixing kit',
+      content:
+        'eum sed dolores ipsam sint possimus debitis occaecati\ndebitis qui qui et\nut placeat enim earum aut odit facilis\nconsequatur suscipit necessitatibus rerum sed inventore temporibus consequatur',
+    },
+    {
+      id: 3,
+      userId: 2,
+      title: 'private piano lessons for beginners',
+      content:
+        'non et quaerat ex quae ad maiores\nmaiores recusandae totam aut blanditiis mollitia quas illo\nut voluptatibus voluptatem\nsimilique nostrum eum',
+    },
+    {
+      id: 4,
+      userId: 2,
+      title: 'online e-book',
+      content:
+        'odit magnam ut saepe sed non qui\ntempora atque nihil\naccusamus illum doloribus illo dolor\neligendi repudiandae odit magni similique sed cum maiores',
+    },
+    {
+      id: 5,
+      userId: 3,
+      title: 'lip mask and scrub',
+      content:
+        'alias dolor cumque\nimpedit blanditiis non eveniet odio maxime\nblanditiis amet eius quis tempora quia autem rem\na provident perspiciatis quia',
+    },
+    {
+      id: 6,
+      userId: 3,
+      title: 'eye cream',
+      content:
+        'debitis eius sed quibusdam non quis consectetur vitae\nimpedit ut qui consequatur sed aut in\nquidem sit nostrum et maiores adipisci atque\nquaerat voluptatem adipisci repudiandae',
+    },
+    {
+      id: 7,
+      userId: 4,
+      title: 'variety tea box',
+      content:
+        'deserunt eos nobis asperiores et hic\nest debitis repellat molestiae optio\nnihil ratione ut eos beatae quibusdam distinctio maiores\nearum voluptates et aut adipisci ea maiores voluptas maxime',
+    },
+    {
+      id: 8,
+      userId: 4,
+      title: 'wildflower honey',
+      content:
+        'rerum ut et numquam laborum odit est sit\nid qui sint in\nquasi tenetur tempore aperiam et quaerat qui in\nrerum officiis sequi cumque quod',
+    },
+    {
+      id: 9,
+      userId: 5,
+      title: 'dozen roses bouquet',
+      content:
+        'ea velit perferendis earum ut voluptatem voluptate itaque iusto\ntotam pariatur in\nnemo voluptatem voluptatem autem magni tempora minima in\nest distinctio qui assumenda accusamus dignissimos officia nesciunt nobis',
+    },
+    {
+        id: 10,
+        userId: 5,
+        title: 'custom bouquet',
+        content:
+          'non et quaerat ex quae ad maiores\nmaiores recusandae totam aut blanditiis mollitia quas illo\nut voluptatibus voluptatem\nsimilique nostrum eum',
+      },
+  ];
+  
+  module.exports = posts;
