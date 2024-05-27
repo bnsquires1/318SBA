2d871d5945b825daba7e7a4bf3d37bcb2b9f781e
Brittany Nicole <bnsquires1@gmail.com>
2024-05-26 22:47:25 -0400

    creating files and data

---
+++ data/users.js
@@ -0,0 +1,37 @@
+// Businesses utilizing the app
+
+const businesses = [
+    {
+      id: 1,
+      name: 'Brittany',
+      company: 'LuxeElixirs',
+      website: 'luxeelixirsbar.com',
+    },
+    {
+        id: 2,
+        name: 'Sedrick',
+        company: 'FirstNote',
+        website: 'firstnoteacademy.com',
+      },
+      {
+        id: 3,
+        name: 'Kamaria',
+        company: 'OnyxBeauty',
+        website: 'onyxbeauty.com',
+      },
+      {
+        id: 4,
+        name: 'Jaye',
+        company: 'TeaAmour',
+        website: 'teaamour.com',
+      },
+      {
+        id: 5,
+        name: 'Ashanti',
+        company: 'Four12Florals',
+        website: 'four12florals.com',
+      },
+  ];
+  
+  module.exports = businesses;
+
